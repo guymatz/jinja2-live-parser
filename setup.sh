a4ad616b10959400a1eba22d0782112ddb5f3cf3
Guy Matz <guy.matz@groupm.com>
2018-01-24 11:20:37 -0500

    correction and check for link to ansible code

--- setup.sh
+++ setup.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 V_DIR="venv"
-which virtualenv 2&>1 > /dev/null || echo "Please install virtualenv" && exit 1
+which virtualenv 2>&1 > /dev/null || echo "Please install virtualenv" && exit 1
 [ -d $V_DIR ] || virtualenv -p python $V_DIR
 source ./$V_DIR/bin/activate
 pip install --upgrade pip
 pip install -r requirements.txt
 SITE_DIR=$(python -c "import sys; print([ x for x in sys.path if x.endswith('site-packages') ][0])")
-ln -s $SITE_DIR/ansible/plugins/filter filters/ansible
+[ -h filters/ansible ] || ln -s $SITE_DIR/ansible/plugins/filter filters/ansible
